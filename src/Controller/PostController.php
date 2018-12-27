73be18e98d8276ad008a47171686205dc6419729
joseph <jturpin99@gmail.com>
2018-12-27 21:41:54 +0100

    Symfony-Ajax Mise en place dynamique de boutons likes au niveau des articles

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Entity\PostLike;
 use App\Repository\PostRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PostLikeRepository;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PostController extends AbstractController
 {
@@ -16,5 +20,51 @@ class PostController extends AbstractController
         return $this->render('post/index.html.twig', [
             'posts' => $repo->findAll(),
         ]);
+    }
+
+    /**
+     * Permet de liker ou unliker un article
+     * 
+     * @Route("/post/{id}/like", name="post_like")
+     * 
+     */
+    public function like(Post $post, ObjectManager $manager, PostLikeRepository $likeRepo)
+    {
+        $user = $this->getUser();
+
+        if(!$user)
+        {
+            return $this->json(['code' => 403, 'message' => 'Unauthorized'], 403);
+        }
+
+        if($post->isLikedByUser($user))
+        {
+            $like = $likeRepo->findOneBy(['post' => $post, 'user' => $user]);
+
+            $manager->remove($like);
+            $manager->flush();
+
+            return $this->json(
+                [
+                    'code' => 200,
+                    'message' => 'Like bien supprimé',
+                    'likes' => $likeRepo->count(['post' => $post])
+                ], 200);
+        }
+
+        $like = new PostLike();
+        $like->setPost($post)
+            ->setUser($user);
+        
+        $manager->persist($like);
+        $manager->flush();
+        
+        
+        return $this->json(
+            [
+                'code' => 200, 
+                'message' => 'Like bien ajouté',
+                'likes' => $likeRepo->count(['post' => $post])
+            ], 200);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,6 +8,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\PostLike;
 
 class AppFixtures extends Fixture
 {
@@ -27,11 +28,26 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create();
 
+        $users = [];
+        
         $user = new User();
         $user->setEmail('user@symfony.com')
             ->setPassword($this->encoder->encodePassword($user, 'password'));
 
         $manager->persist($user);
+
+        $users[] = $user;
+
+        for ($i = 0; $i < 20; $i++)
+        {
+            $user = new User();
+            $user->setEmail($faker->email)
+                ->setPassword($this->encoder->encodePassword($user, 'password'));
+
+            $manager->persist($user);
+
+            $users[] = $user;
+        }
 
         for ($i = 0; $i < 20; $i++) {
             $post = new Post();
@@ -40,6 +56,16 @@ class AppFixtures extends Fixture
                 ->setContent('<p>' . join(',', $faker->paragraphs()) . '</p>');
 
             $manager->persist($post);
+
+            for($j = 0; $j < mt_rand(0, 10); $j++)
+            {
+                $like = new PostLike();
+                $like->setPost($post)
+                    ->setUser($faker->randomElement($users));
+                
+                $manager->persist($like);
+
+            }
         }
 
         $manager->flush();
